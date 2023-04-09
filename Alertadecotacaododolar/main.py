6e47a0e974169c399cbc28b9b39067462bfef77c
Beatrizbrilha <130190318+Beatrizbrilha@users.noreply.github.com>
2023-04-08 21:28:05 -0300

    Initial commit

---
+++ Alertadecotacaododolar/main.py
@@ -0,0 +1,41 @@
+import requests
+
+# Pegar a informação que você quer
+requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL")
+requisicao_dicionario = requisicao.json()
+cotacao = float(requisicao_dicionario['USDBRL']['bid'])
+print(cotacao)
+
+# enviar email
+
+import smtplib
+import email.message
+
+
+def enviar_email(cotacao):
+    corpo_email = f"""
+    <p>Dolar está abaixo de R$5.20. Cotação atual:R${cotacao}</p>
+
+    """
+
+    msg = email.message.Message()
+    msg['Subject'] = "Dólar está abaixo de R$5.20"
+    msg['From'] = 'brilharmeditacao@gmail.com'
+    msg['To'] = 'brilharmeditacao@gmail.com'
+    password = 'uqrrsyuowexecsgd'
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email)
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print('Email enviado')
+
+
+if cotacao < 4.60:
+    enviar_email(cotacao)
+
+# deploy - heroku
+
